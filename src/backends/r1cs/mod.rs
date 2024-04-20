8114a92b226f0e061591da8ae4eb321e6e275e59
kata <kata.choi@gmail.com>
2024-04-20 03:23:17 +0000

    exporting wtns binfile works

--- src/backends/r1cs/mod.rs
+++ src/backends/r1cs/mod.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 
-use ark_bls12_381::{Fr};
+// use num_bigint::{BigUint, ToBigUint};
+
+use ark_bls12_381::Fr;
 use ark_ff::BigInteger;
 use ark_ff::{fields::Field, BigInteger384};
-use num_bigint_dig::BigInt;
+use num_bigint_dig::{BigInt, BigUint};
 
 use crate::{
     helpers::PrettyField,
@@ -14,6 +16,7 @@ use super::Backend;
 
 #[derive(Clone)]
 pub struct R1CS {
+    // pub prime: 
     pub constraints: Vec<Constraint>,
     pub next_variable: usize,
     pub witness_vars: HashMap<usize, Value<R1CS>>,
@@ -177,14 +180,16 @@ impl Backend for R1CS {
 // test
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
+    use std::error::Error;
+    use std::fs::OpenOptions;
+    use std::io::{BufWriter, Seek, SeekFrom, Write};
+    use std::{collections::HashMap, fs::File};
 
     use ark_ff::PrimeField;
     use constraint_writers::r1cs_writer::{
         ConstraintSection, HeaderData, R1CSWriter, SignalSection,
     };
     use kimchi::o1_utils::FieldHelpers;
-    use num_bigint::{BigUint, ToBigInt};
 
     use crate::{constants::Span, var::Value, witness::WitnessEnv};
 
@@ -205,17 +210,24 @@ mod tests {
     }
 
     pub fn port_r1cs(output: &str, r1cs_data: R1CS) -> Result<(), ()> {
-        let prime = BigInt::from_bytes_le(num_bigint_dig::Sign::Plus, &Fr::modulus_biguint().to_bytes_le());
+        let prime = BigInt::from_bytes_le(
+            num_bigint_dig::Sign::Plus,
+            &Fr::modulus_biguint().to_bytes_le(),
+        );
         let field_size = if prime.bits() % 64 == 0 {
             prime.bits() / 8
         } else {
             (prime.bits() / 64 + 1) * 8
         };
 
-        println!("Field size: {}, size in bits: {}", field_size, Fr::size_in_bits());
+        println!(
+            "Field size: {}, size in bits: {}",
+            field_size,
+            Fr::size_in_bits()
+        );
 
-        let r1cs = R1CSWriter::new(output.to_string(), field_size as usize, false)?;
-        let mut constraint_section = R1CSWriter::start_constraints_section(r1cs)?;
+        let r1cs = R1CSWriter::new(output.to_string(), field_size as usize, false).unwrap();
+        let mut constraint_section = R1CSWriter::start_constraints_section(r1cs).unwrap();
         let mut written = 0;
 
         for constraint in r1cs_data.constraints {
@@ -226,12 +238,12 @@ mod tests {
                 &constraint.a.to_bigint_values(),
                 &constraint.b.to_bigint_values(),
                 &constraint.c.to_bigint_values(),
-            )?;
+            );
             written += 1;
         }
 
-        let r1cs = constraint_section.end_section()?;
-        let mut header_section = R1CSWriter::start_header_section(r1cs)?;
+        let r1cs = constraint_section.end_section().unwrap();
+        let mut header_section = R1CSWriter::start_header_section(r1cs).unwrap();
         let header_data = HeaderData {
             field: prime,
             public_outputs: 0,
@@ -241,17 +253,140 @@ mod tests {
             number_of_labels: 0,
             number_of_constraints: written,
         };
-        header_section.write_section(header_data)?;
-        let r1cs = header_section.end_section()?;
-        let mut signal_section = R1CSWriter::start_signal_section(r1cs)?;
+        header_section.write_section(header_data);
+        let r1cs = header_section.end_section().unwrap();
+        let mut signal_section = R1CSWriter::start_signal_section(r1cs).unwrap();
 
         for id in r1cs_data.witness_vars.keys() {
-            SignalSection::write_signal_usize(&mut signal_section, *id)?;
+            SignalSection::write_signal_usize(&mut signal_section, *id);
         }
-        let r1cs = signal_section.end_section()?;
-        R1CSWriter::finish_writing(r1cs)?;
+        let r1cs = signal_section.end_section().unwrap();
+        R1CSWriter::finish_writing(r1cs);
 
         Ok(())
+    }
+
+    struct WitnessWriter {
+        inner: BufWriter<File>,
+        writing_section: Option<WritingSection>,
+        section_size_position: u64,
+    }
+
+    struct WritingSection;
+
+    impl WitnessWriter {
+        // Initialize a FileWriter
+        pub fn new(
+            path: &str,
+            file_type: &str,
+            version: u32,
+            n_sections: u32,
+        ) -> Result<WitnessWriter, ()> {
+            let file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(path)
+                .unwrap();
+            let mut writer = BufWriter::new(file);
+
+            // Write the file type (magic string) as bytes
+            let file_type_bytes = file_type.as_bytes();
+            if file_type_bytes.len() != 4 {
+                panic!("File type must be 4 characters long");
+            }
+            writer.write_all(file_type_bytes);
+
+            // Write the version as a 32-bit unsigned integer in little endian
+            writer.write_all(&version.to_le_bytes());
+
+            // Write the number of sections as a 32-bit unsigned integer in little endian
+            writer.write_all(&n_sections.to_le_bytes());
+
+            let current_position = writer.stream_position().unwrap();
+
+            Ok(WitnessWriter {
+                inner: writer,
+                writing_section: None,
+                section_size_position: current_position,
+            })
+        }
+
+        // Start a new section for writing
+        pub fn start_write_section(&mut self, id_section: u32) -> Result<(), ()> {
+            // if self.writing_section.is_some() {
+            //     return Err(anyhow::anyhow!("Already writing a section"));
+            // }
+
+            self.inner.write_all(&id_section.to_le_bytes()); // Write the section ID as ULE32
+            self.section_size_position = self.inner.stream_position().unwrap(); // Get the current position
+            self.inner.write_all(&0u64.to_le_bytes()); // Temporarily write 0 as ULE64 for the section size
+            self.writing_section = Some(WritingSection);
+
+            Ok(())
+        }
+
+        // End the current section
+        pub fn end_write_section(&mut self) -> Result<(), ()> {
+            let current_pos = self.inner.stream_position().unwrap();
+            let section_size = current_pos - self.section_size_position - 8; // Calculate the size of the section
+
+            self.inner.seek(SeekFrom::Start(self.section_size_position)); // Move back to where the size needs to be written
+            self.inner.write_all(&section_size.to_le_bytes()); // Write the actual section size
+            self.inner.seek(SeekFrom::Start(current_pos)); // Return to the end of the section
+            self.inner.flush(); // Flush the buffer to ensure all data is written to the file
+
+            self.writing_section = None;
+
+            Ok(())
+        }
+        pub fn write(&mut self, witness: &GeneratedWitness, prime: BigInt) -> Result<(), ()> {
+            self.start_write_section(1);
+            let n8 = ((prime.bits() - 1) / 64 + 1) * 8;
+            self.inner.write_all(&(n8 as u32).to_le_bytes());
+            self.write_big_int(prime, n8);
+            self.inner.write_all(&(witness.witness.len() as u32).to_le_bytes());
+            
+            self.end_write_section();
+
+            self.start_write_section(2);
+
+            let sorted_witness_id = witness.witness.keys().collect::<Vec<_>>();
+
+            let sorted_witness = sorted_witness_id
+                .iter()
+                .map(|id| witness.witness.get(id).unwrap())
+                .collect::<Vec<_>>();
+            // map to big int
+            // BigInt::from_bytes_le(num_bigint_dig::Sign::Plus, &val.into_repr().to_bytes_le())
+            let witness = sorted_witness
+                .iter()
+                .map(|val| {
+                    BigInt::from_bytes_le(
+                        num_bigint_dig::Sign::Plus,
+                        &val.into_repr().to_bytes_le(),
+                    )
+                })
+                .collect::<Vec<_>>();
+
+            for value in witness {
+                self.write_big_int(value, n8 as usize);
+            }
+            self.end_write_section();
+
+            Ok(())
+        }
+        // Function to write a BigInt to the file
+        fn write_big_int(&mut self, value: BigInt, size: usize) {
+            let bytes = value.to_bytes_le().1;
+            // if bytes.len() > size {
+            //     return Err(anyhow::anyhow!("Big integer too large for specified size"));
+            // }
+
+            let mut buffer = vec![0u8; size];
+            buffer[..bytes.len()].copy_from_slice(&bytes);
+            self.inner.write_all(&buffer);
+        }
     }
 
     #[test]
@@ -321,6 +456,16 @@ mod tests {
 
         let output_file = "./test.r1cs";
         port_r1cs(&output_file, r1cs);
+        println!("R1CS file written to {}", output_file);
+
+        let mut witness_writer = WitnessWriter::new("./test.wtns", "wtns", 2, 2).unwrap();
+
+        let prime = BigInt::from_bytes_le(
+            num_bigint_dig::Sign::Plus,
+            &Fr::modulus_biguint().to_bytes_le(),
+        );
+        witness_writer.write(&generated_witness, prime);
+        println!("Witness file written to ./test.wtns");
 
         // c + e = f
         // let f = CellVar::new(5, Span::default());
